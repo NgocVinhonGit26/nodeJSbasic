079e854ce5fb7335335d88fc6370005435b3b18a
unknown <Vinh.pn260701@gmail.com>
2022-02-28 21:46:02 +0700

    connectDB

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,26 @@
+import { json } from 'body-parser'
+import connection from '../configs/connectDB'
+
 let getHomepage = (req, res) => {
-    return res.render('index.ejs')
+    let data = []
+
+    connection.query(
+        'SELECT * FROM `users` ',
+        function (err, results, fields) {
+            console.log('>>> check mysql')
+            console.log(results); // results contains rows returned by server
+            results.map((row) => {
+                data.push({
+                    id: row.id,
+                    firstName: row.firstName,
+                    lastName: row.lastName,
+                    email: row.email,
+                    address: row.address
+                })
+            });
+            return res.render('index.ejs', { dataUser: JSON.stringify(data) })
+
+        })
 }
 
 module.exports = {
